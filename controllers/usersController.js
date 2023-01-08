e01f265acab4f4e85c7e7a26e5fc34e6dd4024d1
fun_actuator <kuldeep.msvm@gmail.com>
2023-01-08 17:21:34 +0530

    user controller methods wip

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -3,17 +3,55 @@ const Note = require('../models/Note');
 const asyncHandler = require('express-async-handler');
 const bcrypt = require('bcrypt');
 
+const SALT_ROUNDS = 10;
+
 // @desc Get all users
 // @route GET /users
 //@access Private
 const getAllUsers = asyncHandler(async (req, res) => {
-
+  const users = await User.find().select('-password').lean();
+  if(!users){
+    return res.status(400).json({message:'No users found'})
+  }
+  res.json(users);
 })
 
 // @desc create new user
 // @route POST /users
 //@access Private
 const createUser = asyncHandler(async (req, res) => {
+  //destructure body data
+  const {username, password, roles} = req.body;
+
+  //Confirm Data
+  if(!username || !password || !Array.isArray(roles) || !roles.length){
+    return res.status(400).json({message:'All fields are required'});
+  }
+
+  //check for duplicate
+  const duplicate = await User.findOne({username}).lean().exec();
+
+  //Q --> Why exec? something related to promises
+
+  if(duplicate){
+    return res.status(409).json({message:'Duplicate username'});
+  }
+
+  //Hash password
+
+  const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS) //salt rounds;
+
+  const userObject = {username, 'password':hashedPassword, roles};
+
+  //create and store new user
+
+  const user = await User.create(userObject);
+
+  if(user){ //created
+    res.status(201).json({message:`New user ${username} created`})
+  }else{
+    res.status(400).json({message:'Invalid user data recieved'});
+  }
 
 })
 
