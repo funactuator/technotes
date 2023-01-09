7bd381d3f13f17a1539577d9478de8203bad6f10
fun_actuator <kuldeep.msvm@gmail.com>
2023-01-09 22:35:32 +0530

    update users and delete users api

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -10,7 +10,7 @@ const SALT_ROUNDS = 10;
 //@access Private
 const getAllUsers = asyncHandler(async (req, res) => {
   const users = await User.find().select('-password').lean();
-  if(!users){
+  if(!users?.length){
     return res.status(400).json({message:'No users found'})
   }
   res.json(users);
@@ -59,6 +59,44 @@ const createUser = asyncHandler(async (req, res) => {
 // @route PATCH /users
 //@access Private
 const updateUser = asyncHandler(async (req, res) => {
+  const {id, username, roles, active, password} = req.body;
+
+  // Note: This is patch call, any or all of the above params can be updated except id.
+
+  //Confirm data
+  if(!id || !username || !Array.isArray(roles) || !roles.length || typeof active !== 'boolean'){
+    return res.status(400).json({message:'All fields are required'});
+  }
+
+  const user = await User.findById(id).exec();
+
+  if(!user){
+    return res.status(400).json({message:"User Not Found"});
+  }
+
+  //check for duplicating username if updating username
+  //two cases : username is updated or it is not
+  //but we are checking for duplicacy always
+
+  const duplicate = await User.findOne({username}).lean().exec();
+
+  if(duplicate && duplicate?._id.toString()!==id){
+    return res.status(409).json({message:'duplicate username'});
+  }
+
+  user.username = username;
+  user.roles = roles;
+  user.active = active;
+
+  if(password){
+    const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS); // salt rounds
+    user.password = hashedPassword;
+  }
+
+  const updatedUser = await user.save();
+
+  res.json({message:`${updatedUser.username}updated`});
+
 
 })
 
@@ -66,7 +104,27 @@ const updateUser = asyncHandler(async (req, res) => {
 // @route DELETE /users
 //@access Private
 const deleteUser = asyncHandler(async (req, res) => {
+  const {id} = req.body;
+  if(!id){
+    return res.status(400).json({message:'User Id Required'});
+  }
 
+  const note = await Note.findOne({user:id}).lean().exec();
+
+  if(note){
+    return res.status(400).json({message:'User has assigned notes'});
+  }
+
+  const user = await User.findOne(id).exec();
+  if(!user){
+    return res.status(400).json({message:'User not found'});
+  }
+
+  const result = await user.deleteOne();
+
+  const reply = `Username ${result.username} with ID ${result._id}`;
+
+  res.json({message:reply});
 })
 
 module.exports = {
